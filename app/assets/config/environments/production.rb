5bdcc85905203932f36c3fd95e36738283e714b4
chantake1128 <chantake1128@gmail.com>
2024-08-15 12:00:27 +0900

    Create production.rb

---
+++ app/assets/config/environments/production.rb
@@ -0,0 +1,11 @@
+Rails.application.configure do
+  config.cache_classes = true
+  config.eager_load = true
+  config.consider_all_requests_local = false
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.assets.compile = false
+  config.assets.digest = true
+  config.log_level = :debug
+  config.i18n.fallbacks = true
+  config.active_support.deprecation = :notify
+end
